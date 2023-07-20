66e1d3dd598bfdb70f6908b8bb1561f1b165c08c
S_HYEON <zxl3651@naver.com>
2023-07-20 22:19:27 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 348 ms, Memory: 53596 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -6,18 +6,7 @@ x = []
 for i in range(N):
     num = list(map(int, input().split()))
     x.append(num)
-arr = sorted(x, key=lambda x:x[0])
-tmp = arr[0][0]
-array = []
+arr = sorted(x, key=lambda x:x[1])
+arr = sorted(arr, key=lambda x:x[0])
 for i in arr:
-    if tmp == i[0]:
-        array.append(i)
-    elif tmp != i[0]:
-        array = sorted(array, key=lambda x: x[1])
-        for j in array:
-            print(j[0], j[1])
-        array = []
-        tmp = i[0]
-        array.append(i)
-for i in sorted(array, key=lambda x: x[1]):
     print(i[0], i[1])
