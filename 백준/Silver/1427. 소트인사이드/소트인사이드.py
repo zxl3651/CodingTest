3f7fb333bd1eec32695096b1586ac13e01cf701b
S_HYEON <zxl3651@naver.com>
2023-07-20 14:56:48 +0900

    [Silver V] Title: 소트인사이드, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.py
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.py
@@ -1,15 +1,4 @@
 import sys
 input = sys.stdin.readline
 
-N = int(input())
-arr = []
-while(N > 10):
-    n = N%10
-    arr.append(n)
-    N //= 10
-arr.append(N)
-arr = sorted(arr, reverse=True)
-answer = ''
-for i in arr:
-    answer += str(i)
-print(answer)
+print("".join(sorted(input(), reverse=True)))
