31a59aa31f5c02a004e9d6f664a41ea71eb89f3b
S_HYEON <zxl3651@naver.com>
2023-07-23 02:12:15 +0900

    [Silver IV] Title: 제로, Time: 80 ms, Memory: 34160 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,14 @@
+import sys
+from math import factorial
+
+input = sys.stdin.readline
+
+k = int(input())
+stack = []
+for i in range(k):
+    n = int(input())
+    if n == 0:
+        stack.pop()
+    else:
+        stack.append(n)
+print(sum(stack))
